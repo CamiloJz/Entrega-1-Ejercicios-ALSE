948fe540245122f7294c17e0b54e701c30368c48
camilojz <camilo.jimenez-o@mail.escuelaing.edu.co>
2025-02-26 20:58:24 -0500

    Punto 22

--- Ejercicio22.cpp
+++ Ejercicio22.cpp
@@ -7,11 +7,31 @@ using namespace std;
 int main()
 {
     int a = 0;
-    cout << "Ingrese su numero hola";
+    bool res = false;
+    cout << "Ingrese su numero: ";
     cin >> a;
-    if (a > 0){
-        
-        cout << "Su numero es positivo" << endl;
+    if (a > 0 && (a % 2) == 0){
+        if (!res == true)
+        {
+            cout << "Resultado: Es positivo y par ";
+        }
+    }
+    else if (a > 0 && (a % 2) != 0)
+    {
+        if (!res == true)
+        {
+            cout << "Resultado: Es positivo e impar ";
+        }
+    }
+    else if (a < 0 && (a % 2) != 0)
+    {
+        if (!res == true)
+        {
+            cout << "Resultado: Es Negativo e impar ";
+        }
+    }
+    else {
+        cout << "Resultado: Es negativo y par";
     }
 
 return 0;        
